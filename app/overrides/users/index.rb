6231db29fa33791b099c36fcc83f6d06214e0b55
jresinas <jresinas@emergya.com>
2015-12-01 13:25:54 +0100

    Plugin localization

--- app/overrides/users/index.rb
+++ app/overrides/users/index.rb
@@ -2,4 +2,4 @@ Deface::Override.new :virtual_path  => 'issues/index',
                      :name          => 'issues-summatory',
                      :original		=> 'a225e026cb9e5de91cf8bc562f1c66c17f4220a2',
                      :insert_before	=> "erb[loud]:contains('link_to_function l(:button_apply)')",
-                     :text			=> '<h3><span class="total_estimated">Total estimado: <%= @issues.inject(0.0){|sum,i| sum+i.estimated_hours.to_f} %></span></h3>'
+                     :text			=> '<h3><span class="total_estimated"><%= l(:"emergya_agile.total_scheduled")+": "+@issues.inject(0.0){|sum,i| sum+i.estimated_hours.to_f}.to_s %></span></h3>'
